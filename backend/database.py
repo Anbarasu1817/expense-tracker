3c43664525b9bd70b37077b86a23b336d9a2a9a0
Anbarasu1817 <anbarasu1817@gmail.com>
2026-06-05 16:13:30 +0530

    Updated database connection

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,10 @@
 import os
 from sqlalchemy import create_engine, text
 
-DATABASE_URL = "postgresql://postgres:anbu18@localhost:5432/expense_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:anbu18@localhost:5432/expense_db"
+)
 
 engine = create_engine(DATABASE_URL)
 
