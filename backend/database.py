ecfb6afee145ad7ddc6cd6af283a34f665547522
Anbarasu1817 <anbarasu1817@gmail.com>
2026-06-12 15:56:20 +0530

    Expense tracker login fix

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,7 @@
 import os
 from sqlalchemy import create_engine, text
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:anbu18@localhost:5432/expense_db"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
